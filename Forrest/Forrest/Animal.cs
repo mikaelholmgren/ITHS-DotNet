6682527d10c983cf67b6e591182385eb1dc0c14f
Mikael Holmgren <mikael.holmgren@iths.se>
2020-09-17 10:21:24 +0200

    Forrest project

---
+++ Forrest/Forrest/Animal.cs
@@ -0,0 +1,23 @@
+﻿namespace Forrest
+{
+    internal class Animal
+    {
+        public string Name { get; set; }
+        public bool Nocturnal { get; set; }
+        public bool HaveWings { get; set; }
+        public string CustomAction { get; set; }
+        public string GetActionMessage(bool day)
+        {
+            string actionString = CustomAction != null ? CustomAction : $"{Name}en springer omkring och letar mat";
+            bool shouldBeAwake = false;
+            if (!Nocturnal && day) shouldBeAwake = true;
+            else if (Nocturnal && !day) shouldBeAwake = true;
+            // i annat fall så är det sover
+            if (HaveWings) actionString = $"{Name}en flyger omkring och letar mat";
+            else if (Nocturnal) actionString = $"{Name} smyger omkring och jagar sitt byte";
+            if (shouldBeAwake) return actionString;
+            else return $"{Name}en sover";
+        }
+
+    }
+}
---
+++ Forrest/Forrest/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Forrest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool running = true;
+            List<Animal> forrest = new List<Animal>();
+            forrest.Add(CreateAnimalWithProps("Fladdermus", true, true));
+            forrest.Add(CreateAnimalWithProps("Varg", true, false));
+            forrest.Add(CreateAnimalWithProps("Örn", false, true));
+            forrest.Add(CreateAnimalWithProps("häst", false, false, "galopperar omkring och har kul"));
+            while (running) 
+            {
+                Console.Write("Välkommen till skogen! Tryck d för dag eller n för natt");
+                char key = Console.ReadKey(true).KeyChar;
+                Console.WriteLine();
+                switch (key)
+                {
+                    case 'd':
+                        foreach (var animal in forrest)
+                        {
+                            Console.WriteLine(animal.GetActionMessage(true));
+                            
+
+                        }
+                        break;
+                    case 'n':
+                        foreach (var animal in forrest)
+                        {
+                            Console.WriteLine(animal.GetActionMessage(false));
+
+
+                        }
+                        break;
+                    case 'a':
+                        running = false;
+                        Console.WriteLine("Du lämnar nu skogen, hejdå!");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private static Animal CreateAnimalWithProps(string name, bool nocturnal, bool wings, string custom = null)
+        {
+            Animal animal = new Animal();
+            animal.Name = name;
+            animal.Nocturnal = nocturnal;
+            animal.HaveWings = wings;
+            if (custom != null)
+                animal.CustomAction = $"{name}en " + custom;
+            return animal;
+        }
+    }
+}
